c3efc0cef56b76def20c69ff60c463400523ba66
Gabriel Finatti <ghfinatti3@gmail.com>
2021-08-17 20:31:57 -0300

    add markers to DOM with click

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-const gameModule = (() => {
-    let gameboard = [1,2,3,4,5,6,7,8,9];
-    return {gameboard}
+const game = (() => {
+    let players = [];
+    let gameboard = ["","","","","","","","",""];
+  
+    countX = gameboard.filter(marker => marker === "X").length
+    countO = gameboard.filter(marker => marker === "O").length
+
+    return { gameboard, players }
+
 })();
 
-const createPlayer = (name) => {
-    const sayHello = () => {
-        console.log(`hello ${name}`)
+const displayController = (() => {
+    boardSquares = document.querySelectorAll('.board-squares');
+    
+    const fillBoard = (boardSquares) => {
+        for (let i = 0; i < boardSquares.length; i++){
+            boardSquares[i].textContent = game.gameboard[i];
+        }
     }
-    return { name, sayHello };
+
+    boardSquares.forEach( (square) => {
+        square.addEventListener('click', (e) => {
+            squarePosition = e.target.dataset.square;
+            if (countX === countO && square.textContent === ""){
+                game.gameboard[squarePosition] = "X";
+                square.textContent = "X"
+                console.log(game.gameboard);
+            };
+            if (countX > countO && square.textContent === ""){
+                game.gameboard[squarePosition] = "O";
+                square.textContent = "O"
+                console.log(game.gameboard);
+            };
+            countX = game.gameboard.filter(marker => marker === "X").length
+            countO = game.gameboard.filter(marker => marker === "O").length
+        });
+    });
+
+    return { fillBoard }
+
+})();
+
+const createPlayer = (name, marker) => {
+    game.players.push({name, marker});
+
+    return { name, marker }
 }
 
-const gabriel = createPlayer('Gabriel');
-const jonas = createPlayer(`Jonas`);
+const gabriel = createPlayer('Gabriel', 'X');
+const jonas = createPlayer('Jonas', 'O');
